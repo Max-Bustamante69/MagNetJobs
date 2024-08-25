8ad386facd9d0126a9cfdd2b89ae319b78b21bd9
Max-Bustamante69 <mbustamang@eafit.edu.co>
2024-08-25 12:15:31 -0500

    REST API CONFIGURATION

---
+++ backend/magnetJobs/urls.py
@@ -0,0 +1,12 @@
+from rest_framework import routers
+from .api import UserViewSet
+
+router = routers.DefaultRouter()
+router.register('api/users', UserViewSet, 'users')
+
+
+
+
+
+
+urlpatterns = router.urls
