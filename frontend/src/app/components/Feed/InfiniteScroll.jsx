ab40f051d89c710aaf953b0196f980df5aba9552
SofiAlfonso <asalfonsom@eafit.edu.co>
2024-09-20 23:07:51 -0500

    Modificación de aspecto de la lista de posts

--- frontend/src/app/components/Feed/InfiniteScroll.jsx
+++ frontend/src/app/components/Feed/InfiniteScroll.jsx
@@ -1,4 +1,6 @@
 // pages/posts.jsx
+import formatDate from '../../../utils/FormatDate'
+import { loadUsers } from '../../../utils/LoadUsers'
 
 async function loadPosts()
 {
@@ -8,26 +10,35 @@ async function loadPosts()
 }
 
 async function ListPosts() {
-    const posts= await loadPosts()
-    console.log(posts)
-  
+    const posts= await loadPosts();
+    const users = await loadUsers();
+
+    // Función para obtener el nombre del usuario basado en el user_id
+    const getUserById = (userId) => {
+        const user = users.find((user) => user.id === userId);
+        return user ? user.username : "Unknown User";
+    };
+    
   return (
-    <div className="bg-slate-700 p-4  ">
-        <h1>POSTS</h1>
+    <div className="flex flex-col items-center">
         {posts.map(post =>(
-            <div key={post.id} className="bg-slate-500 px-4 py-3 mb-2 ">
+            <div key={post.id} className="bg-neutral-900 px-4 py-3 mb-2 w-1/2  flex flex-col justify-around space-y-3">
+                <h1 className='text-lg font-semibold italic'>{getUserById(post.user)}</h1>
+                
+                <div className='flex justify-center'>
+                    {post.image ? (
+                            <img
+                                src={`${post.image}`}
+                                alt="Post image"
+                                className="w-1/2 h-auto mb-2"
+                                onError={(e) => console.log("Error loading image", e.target.src)}
+                            />
+                        ) : (
+                            <p></p>
+                        )}
+                </div>
                 <h2>{post.content}</h2>
-                {post.image ? (
-                        <img
-                            src={`${post.image}`}
-                            alt="Post image"
-                            className="max-w-full h-auto mb-2"
-                            onError={(e) => console.log("Error loading image", e.target.src)}
-                        />
-                    ) : (
-                        <p>______</p>
-                    )}
-                <p>{post.created_at}</p>
+                <p className='font-extralight text-xs'>{formatDate(post.created_at)}</p>
             </div>
         ))}
 
