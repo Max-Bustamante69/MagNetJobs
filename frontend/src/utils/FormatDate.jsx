113e8c311a689630feecc34a5a7b6ed85867733f
SofiAlfonso <asalfonsom@eafit.edu.co>
2024-09-20 23:06:20 -0500

    Creacion de función que formatea la fecha

---
+++ frontend/src/utils/FormatDate.jsx
@@ -0,0 +1,17 @@
+
+function formatDate(isoString)
+{   const date = new Date(isoString);
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    } 
+    ).format(date);
+    
+}
+
+export default formatDate;
