85d5728e31e3c871858ed77c8ae979312d90b9c1
SofiAlfonso <asalfonsom@eafit.edu.co>
2024-09-20 23:22:58 -0500

    Modificación función de loadposts para refrescarlos automaticamente y organización de posts por fecha de publicación

--- frontend/src/app/components/Feed/InfiniteScroll.jsx
+++ frontend/src/app/components/Feed/InfiniteScroll.jsx
@@ -4,9 +4,25 @@ import { loadUsers } from '../../../utils/LoadUsers'
 
 async function loadPosts()
 {
-    const res= await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts/`)
-    const data= await res.json()
-    return data
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts/`, {
+            method: 'GET',
+            headers: {
+                'Cache-Control': 'no-store',  // Ensure no caching
+            },
+            cache: 'no-store',  // Also prevent fetch from caching
+        });
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        return [];
+    }
 }
 
 async function ListPosts() {
@@ -19,10 +35,12 @@ async function ListPosts() {
         return user ? user.username : "Unknown User";
     };
     
+    posts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    
   return (
     <div className="flex flex-col items-center">
         {posts.map(post =>(
-            <div key={post.id} className="bg-neutral-900 px-4 py-3 mb-2 w-1/2  flex flex-col justify-around space-y-3">
+            <div key={post.id} className="bg-neutral-900 px-4 py-3 mb-2 rounded-md w-1/2  flex flex-col justify-around space-y-3">
                 <h1 className='text-lg font-semibold italic'>{getUserById(post.user)}</h1>
                 
                 <div className='flex justify-center'>
