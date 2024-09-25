ca363b808a9a7ce8aafb91f02e0ffd22c58ba233
vvhornung <vvhornung@eafit.edu.co>
2024-09-25 01:35:39 -0500

    entrega parcial perfil de usuario

--- frontend/src/app/components/Profile/UserCard.jsx
+++ frontend/src/app/components/Profile/UserCard.jsx
@@ -5,48 +5,53 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
  function UserCard({ user }) {
-
+    console.log(user)
+    const dummieUser = {
+        username: 'Carlos',
+        first_name: 'Carlos',
+        last_name: 'Nava',
+        employed: true
+    }
     return (
+      <div className="relative rounded-xl w-full group">
+        <Card className="relative bg-black border-gray-400 flex justify-around p-8 z-10 ">
+          <div className="flex flex-col items-center justify-center">
+            <Avatar className="size-40">
+              <AvatarImage src="https://images.squarespace-cdn.com/content/v1/606d159a953867291018f801/1619987722169-VV6ZASHHZNRBJW9X0PLK/Key_Art_02_layeredjpg.jpg?format=1500w" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="flex flex-col gap-y-4">
+            <div className="flex justify-around">
+              <h2 className="text-white text-4xl font-bold">
+                {dummieUser.username.toUpperCase()}
+              </h2>
 
-        <div className='relative rounded-xl w-full group'>
-          <Card className="relative bg-black border-gray-400 flex justify-around p-8 z-10 ">
-            <div className='flex flex-col items-center justify-center'>
-              <Avatar  className='size-40'>
-                <AvatarImage   src="https://images.squarespace-cdn.com/content/v1/606d159a953867291018f801/1619987722169-VV6ZASHHZNRBJW9X0PLK/Key_Art_02_layeredjpg.jpg?format=1500w" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
+              <Button variant="outline">Editar Perfil</Button>
             </div>
-            <div className='flex flex-col gap-y-4'>
-                <div className='flex justify-around'>
-                    <h2 className='text-white text-4xl font-bold'> 
-                        
-                        {user.username.toUpperCase()}
-    
-                    </h2>
-    
-    
-                    <Button  variant="outline">Editar Perfil</Button>
-    
-    
-                </div>
-                <div className='flex gap-4'>
-                    <p className="text-white"><strong>34</strong> Publicaciones</p>
-                    <p className="text-white"><strong>23</strong> Seguidores</p>
-                    <p className="text-white"><strong>43</strong> Seguidos</p>
-                </div>
-    
-                <div className='flex flex-col gap-4 text-white'>
-                    <p>{user.first_name + ' ' + user.last_name}</p>
-                    <p> {user.employed ? 'Empleado' : 'Desempleado'} </p>
-    
-                </div>
+            <div className="flex gap-4">
+              <p className="text-white">
+                <strong>34</strong> Publicaciones
+              </p>
+              <p className="text-white">
+                <strong>23</strong> Seguidores
+              </p>
+              <p className="text-white">
+                <strong>43</strong> Seguidos
+              </p>
             </div>
-          </Card>
-          <div className='absolute rounded-xl inset-0 bg-gradient-to-r from-blue-600 to-emerald-600 blur-xl opacity-50 group-hover:opacity-100 transition-opacity duration-1000 group-hover:duration-200'></div>
-        </div>
 
+            <div className="flex flex-col gap-4 text-white">
+              <p>{dummieUser.first_name + " " + dummieUser.last_name}</p>
+              <p> {dummieUser.employed ? "Empleado" : "Desempleado"} </p>
+            </div>
+          </div>
+        </Card>
+        <div className="absolute rounded-xl inset-0 bg-gradient-to-r from-white  to-blue-400 blur-xl opacity-50 group-hover:opacity-100 transition-opacity duration-1000 group-hover:duration-200"></div>
+      </div>
 
-        );
+      // from-blue-600 to-emerald-600 bg-clip-text
+    );
 }
 
 export default UserCard;
--- frontend/src/app/profile/page.jsx
+++ frontend/src/app/profile/page.jsx
@@ -6,13 +6,180 @@ async function Profile() {
     const user = users[0]
 
     return (
-        <div className='flex flex-col items-center'>
-            <section className='w-1/2 mt-12 flex flex-col items-center gap-12'>
-            <h2  className='font-extrabold text-7xl'>Your <span className='bg-gradient-to-r text-transparent from-blue-600 to-emerald-600 bg-clip-text'>Profile</span></h2>
-    
-            <UserCard user={user} />
-            </section>
-        </div>
+      <div className="flex flex-col items-center gap-10">
+        <section className="w-2/3 2xl:w-1/2 mt-12 flex flex-col items-center gap-12">
+          <h2 className="font-extrabold text-7xl">
+            Your{" "}
+            <span className="bg-gradient-to-r text-transparent from-white to-blue-400 bg-clip-text">
+              Profile
+            </span>
+          </h2>
+          <UserCard user={user} />
+         
+          <article className="flex w-full ">
+            
+          <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="/docs/images/people/profile-picture-5.jpg" alt="Bordered avatar"/>
+          <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="/docs/images/people/profile-picture-5.jpg" alt="Bordered avatar"/>
+          <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="/docs/images/people/profile-picture-5.jpg" alt="Bordered avatar"/>
+          <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="/docs/images/people/profile-picture-5.jpg" alt="Bordered avatar"/>
+
+          </article>
+
+        </section>
+
+        <section className="w-2/3 2xl:w-1/2 border-t border-white border-opacity-15">
+          <div className="flex justify-center gap-12  ">
+            <div className="flex gap-2  border-t border-white py-4 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+                />
+              </svg>
+              <p className="text-xs">POSTS</p>
+            </div>
+            <div className="flex gap-2 border-t border-white py-4 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                />
+              </svg>
+
+              <p className="text-xs">SAVED</p>
+            </div>
+            <div className="flex gap-2  border-t border-white py-4 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+
+              <p className="text-xs">LIKES</p>
+            </div>
+          </div>
+
+          <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div class="grid gap-4">
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div class="grid gap-4">
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div class="grid gap-4">
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div class="grid gap-4">
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <img
+                  class="h-auto max-w-full rounded-lg"
+                  src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     );
 }
 
