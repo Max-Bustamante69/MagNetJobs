d84e1a3ffb22b15a1a18b3cd319cb161daa0c179
Max-Bustamante69 <mbustamang@eafit.edu.co>
2024-09-25 23:33:08 -0500

    node, tailwind and packages updates

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,22 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['plus.unsplash.com'],
-    },
-    async headers() {
-        return [
-        {
-            source: '/(.*)',
-            headers: [
-            {
-                key: 'X-Frame-Options',
-                value: 'DENY',
-            },
-            ],
-        },
-        ];
-    },
+  reactStrictMode: true,
+  images: {
+    domains: ["plus.unsplash.com", "images.unsplash.com"],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@node-rs/argon2"],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
