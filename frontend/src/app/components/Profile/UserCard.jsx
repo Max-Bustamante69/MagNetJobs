ded4c7d8a9f526b07edb4b4a85096f84230846df
vvhornung <vvhornung@eafit.edu.co>
2024-11-06 19:33:41 -0500

    cambios en la usercard + usuario quemado para manejo de errores

--- frontend/src/app/components/Profile/UserCard.jsx
+++ frontend/src/app/components/Profile/UserCard.jsx
@@ -10,6 +10,16 @@ import Loader from "@/components/ui/loader";
 
 function UserCard({ user }) {
   const [userOnSession, setUserOnSession] = useState(null);
+  user = {
+    id: "c5dswrglyhb5shr2",
+    username: "Laurikisa",
+    email: "test@beispiel.de",
+    avatar_image: null,
+    bio: null,
+    posts: [14],
+    first_name: "Gottfried",
+    last_name: "Leibniz",
+  };
 
   // Cargar usuario en sesión cuando el componente se monte
   useEffect(() => {
@@ -27,44 +37,52 @@ function UserCard({ user }) {
   }
     return (
       <div className="relative rounded-xl w-full group">
-        <Card className="relative bg-black border-gray-400 flex justify-around p-8 z-10 ">
+        <Card className="relative bg-black border-black flex justify-around p-8 z-10 ">
           <div className="flex flex-col items-center justify-center">
-            <Avatar className="size-40">
-              <AvatarImage src="https://images.squarespace-cdn.com/content/v1/606d159a953867291018f801/1619987722169-VV6ZASHHZNRBJW9X0PLK/Key_Art_02_layeredjpg.jpg?format=1500w" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <div className="relative">
+              <div className="size-72 bg-white rounded-full absolute top-0 blur-lg"> 
+
+              </div>
+              <Avatar className="size-72">
+                <AvatarImage src="https://images.squarespace-cdn.com/content/v1/606d159a953867291018f801/1619987722169-VV6ZASHHZNRBJW9X0PLK/Key_Art_02_layeredjpg.jpg?format=1500w" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
           </div>
           <div className="flex flex-col gap-y-4">
-            <div className="flex justify-around">
-              <h2 className="text-white text-4xl font-bold">
-                {user.username.toUpperCase()}
+            <div className="flex justify-between">
+              <h2 className="text-white text-xl font-semibold">
+                {user.username?.toUpperCase()}
               </h2>
               {/*Esta verificación debe cambiarse por el id del usuario con sesión iniciada*/}
-              {user.username== userOnSession.username &&(
-              <Icons IconName={HiOutlinePencilAlt}/>)}
+              {user.username == userOnSession.username && (
+                <Icons IconName={HiOutlinePencilAlt} />
+              )}
             </div>
-            <div className="flex gap-4">
-              <p className="text-white">
+            <div className="flex gap-12">
+              <p className="text-white text-xl font-semibold">
                 <strong>34</strong> Publicaciones
               </p>
-              <p className="text-white">
+              <p className="text-white text-xl font-semibold">
                 <strong>23</strong> Seguidores
               </p>
-              <p className="text-white">
+              <p className="text-white text-xl font-semibold">
                 <strong>43</strong> Seguidos
               </p>
-               {/* Renderizar el botón de seguimiento solo si el usuario no es el mismo */}
-            {user.username !== userOnSession.username && (
-              <FollowButton user={user} userOnSession={userOnSession}  />)}
+              {/* Renderizar el botón de seguimiento solo si el usuario no es el mismo */}
+              {user.username !== userOnSession.username && (
+                <FollowButton user={user} userOnSession={userOnSession} />
+              )}
             </div>
 
-            <div className="flex flex-col gap-4 text-white">
-              <p>{user.first_name + " " + user.last_name}</p>
+            <div className="flex flex-col gap-4 text-white text-xl font-bold ">
+              <p className="font-normal">
+                {user.first_name + " " + user.last_name}
+              </p>
               <p> {user.employed ? "Empleado" : "Desempleado"} </p>
             </div>
           </div>
         </Card>
-        <div className="absolute rounded-xl inset-0 bg-gradient-to-r from-white  to-green-700 blur-xl opacity-50 group-hover:opacity-100 transition-opacity duration-1000 group-hover:duration-200"></div>
       </div>
 
       // from-blue-600 to-emerald-600 bg-clip-text
