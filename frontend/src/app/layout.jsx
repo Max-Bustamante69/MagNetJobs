6785b1f38251e8c69552330d5089389f569b7952
Max-Bustamante69 <mbustamang@eafit.edu.co>
2024-11-07 21:36:32 -0500

    server side layout

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,8 +1,6 @@
-"use client";
-import { Inter } from "next/font/google";
+"use client";import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/app/(main)/components/General/NavBar";
-import { usePathname } from "next/navigation";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
